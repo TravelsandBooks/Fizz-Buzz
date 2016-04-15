f1e9eedff1b9b6a39954e735b0129030d60c7675
Jess <jess.harris91@gmail.com>
2016-04-15 15:57:01 +0100

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -4,7 +4,7 @@ def fb(a):
       elif i % 5 == 0: a[n] = "Buzz"
       elif i % 3 == 0: a[n] = "Fizz"
     return a
-def print_fb(b):
+def print_fb(b):                                        #lists are ugly -- let's make it easier to see.
     for i,n in enumerate(b):
         if (i + 1) % 10 == 0: print str(n) + " \n"      #if it's an index divisible by 10, break the line
         else: print str(n) + " ",                       #otherwise just print the value with a space
