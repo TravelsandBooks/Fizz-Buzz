a2a2bea67b60d9bc20759b0d4cf1ac73cfb8c8cb
Jess <jess.harris91@gmail.com>
2016-04-15 15:56:34 +0100

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,12 @@
 def fb(a):
     for n,i in enumerate(a):
-      if i % 15 == 0: a[n] = "Fizz Buzz"
+      if i % 15 == 0: a[n] = "Fizz Buzz"                #this is just neater -- lowest common denominator
       elif i % 5 == 0: a[n] = "Buzz"
       elif i % 3 == 0: a[n] = "Fizz"
     return a
-
-fizzbuzz = fb([x for x in range(1,101)])
-print fizzbuzz
+def print_fb(b):
+    for i,n in enumerate(b):
+        if (i + 1) % 10 == 0: print str(n) + " \n"      #if it's an index divisible by 10, break the line
+        else: print str(n) + " ",                       #otherwise just print the value with a space
+fizzbuzz = fb([x for x in range(1,101)])                #we're doing it for all numbers from 1 to 100
+print_fb(fizzbuzz)
